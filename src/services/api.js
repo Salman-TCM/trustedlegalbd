07a17d77cf4af965d80f2e437bb0ddd790480e60
Salman-TCM <salman.connects@gmail.com>
2025-10-08 17:57:41 +0600

    fixed: fronend connection issue at docker

--- src/services/api.js
+++ src/services/api.js
@@ -1,9 +1,11 @@
 import axios from 'axios';
 import { API_BASE_URL } from '../config/api';
+import config from '../config';
 
 // Create axios instance
 const api = axios.create({
   baseURL: API_BASE_URL,
+  timeout: config.api.timeout,
   headers: {
     'Content-Type': 'application/json',
   },
